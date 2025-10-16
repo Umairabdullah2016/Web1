be1ab232e39338fa475c1022745e0c1777aaddea
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-16 19:19:52 +0100

    Update page title and main title for Streamlit app

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="All In One Diary of a Wimpy Kid",
+    page_title="Diary Of A Wimpy Kid | The Third Wheel",
     page_icon="📖",  # You can use an emoji or a path to an image file
     layout="wide",   # optional: 'centered' or 'wide'
 )
 
-st.title("All In One Diary of a Wimpy Kid book")
+st.title(Diary of a Wimpy Kid | The Third Wheel")
 
 pdf_url = "https://jumpshare.com/share/OzBRI4N7wDSZX1kXQn9s"
 
