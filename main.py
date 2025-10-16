52484e15554c853822bc035937569185dccf0bc4
Umairabdullah2016 <ua8839705@gmail.com>
2025-10-16 19:08:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 
-st.title("All In One Diary of a wimpy kid book")
+st.title("All In One Diary of a Wimpy Kid book")
 
 pdf_url = "https://jumpshare.com/share/OzBRI4N7wDSZX1kXQn9s"
 
 # Try embedding with iframe
 st.markdown(f'''
-<iframe src="{pdf_url}" width="100%" height="800px")
+<iframe src="{pdf_url}" width="100%" height="800px"></iframe>
+''', unsafe_allow_html=True)
 
 st.write("Credit: Jeff Kinney")
+
